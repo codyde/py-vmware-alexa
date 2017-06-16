885285cf26c8faa697ef3a64eb1e77ebd04d7239
codyde <codydearkland@gmail.com>
2017-06-16 14:07:39 -0700

    REST auth overhaul. Confiured reusing of existing SID, removal of expired sid, still testing

--- vmapi.py
+++ vmapi.py
@@ -55,8 +55,12 @@ def get_rest_api_data(req_url):
     resp = requests.get(req_url, verify=False,
                         headers={'vmware-api-session-id': sid})
     if resp.status_code != 200:
+        if resp.status_code == 401:
+            print("401 received; clearing stale SID")
+            AuthConfig.remove_option('auth', 'sid')
+            AuthConfig.remove_section('auth')
         print('Error! API responded with: {}'.format(resp.status_code))
-        newsid = auth_vcenter_rest()
+        auth_vcenter_rest()
         get_rest_api_data(req_url)
         return
     return resp
