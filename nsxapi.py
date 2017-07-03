6ef64bdc5b7be4ac1f9a81666565faa3dc782d5f
codyde <codydearkland@gmail.com>
2017-07-03 16:44:31 -0700

    Added code to enable NSX to check API and create a virtual wire

--- nsxapi.py
+++ nsxapi.py
@@ -17,10 +17,36 @@ def validateNSX():
         "accept":"application/json",
         'content-type':'application/json'
     }
-    r = requests.get(nsxUrl, headers=headers, auth=HTTPBasicAuth(user, password), verify=False)
-    if r.status_code != 200: 
+    res = requests.get(nsxUrl, headers=headers, auth=HTTPBasicAuth(user, password), verify=False)
+    if res.status_code != 200: 
         return("API Down")
     else:
         return("API is Online")
+
+def createNsxWire(lsName):
+    Config = configparser.ConfigParser()
+    Config.read("/srv/avss/appdata/etc/config.ini")
+    url = Config.get("nsxConfig", "url")
+    user = Config.get("nsxConfig", "user")
+    password = Config.get("nsxConfig", "password")
+    tenant = Config.get("nsxConfig", "tenant")
+    nsxUrl = "{}api/2.0/vdn/scopes/vdnscope-1/virtualwires".format(url)
+    xml = """<virtualWireCreateSpec>
+<name>{}</name>
+<description></description>
+<tenantId></tenantId>
+<controlPlaneMode>UNICAST_MODE</controlPlaneMode>
+<guestVlanAllowed>false</guestVlanAllowed>
+</virtualWireCreateSpec>""".format(lsName)
+    headers = {
+        "accept":"application/xml",
+        'content-type':'application/xml'
+    }
+    res = requests.post(nsxUrl, headers=headers, auth=HTTPBasicAuth(user, password), data=xml, verify=False)
+    if res.status_code != 201:
+        return "Error Occured During Creation"
+    else: 
+        return lsName+" successfully created"
+
 
         
